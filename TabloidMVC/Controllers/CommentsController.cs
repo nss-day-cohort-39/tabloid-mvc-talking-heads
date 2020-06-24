8c35faceb141efb3d5191e7270c3b735dcefc2f1
Alex Curnow <alexcurnow@gmail.com>
2020-06-24 14:24:48 -0500

    began work on comments

---
+++ TabloidMVC/Controllers/CommentsController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using TabloidMVC.Repositories;
+
+namespace TabloidMVC.Controllers
+{
+    [Authorize]
+    public class CommentsController : Controller
+    {
+        private readonly CommentRepository _commentRepository;
+
+        public CommentsController(IConfiguration config)
+        {
+            _commentRepository = new CommentRepository(config);
+        }
+        // GET: CommentsController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CommentsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CommentsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CommentsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CommentsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CommentsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CommentsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CommentsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ TabloidMVC/Repositories/CommentRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TabloidMVC.Models;
+using Microsoft.Data.SqlClient;
+
+
+namespace TabloidMVC.Repositories
+{
+    public class CommentRepository : BaseRepository
+    {
+        public CommentRepository(IConfiguration config) : base(config) { }
+        public List<Comment> GetCommentsByPostId(int postId)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                                        SELECT Id, PostId, UserProfileId, Subject, Content, CreateDateTime
+                                        FROM Comment
+                                        WHERE PostId = @postId
+                                        ";
+                    var reader = cmd.ExecuteReader();
+                    var comments = new List<Comment>();
+
+                    while (reader.Read())
+                    {
+                        comments.Add(new Comment()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            PostId = reader.GetInt32(reader.GetOrdinal("PostId")),
+                            UserProfileId = reader.GetInt32(reader.GetOrdinal("UserProfileId")),
+                            Subject = reader.GetString(reader.GetOrdinal("Subject")),
+                            Content = reader.GetString(reader.GetOrdinal("Content")),
+                            CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime"))
+                        });
+                    }
+
+                    reader.Close();
+
+                    return comments;
+                }
+            }
+        }
+    }
+}
