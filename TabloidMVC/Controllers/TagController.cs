5142452d2ca268b01f814c5cc10530d1fd6157a2
Billy Blackman <blackmanbilly@gmail.com>
2020-06-24 11:33:04 -0500

    Added alphabetized tag index

---
+++ TabloidMVC/Controllers/TagController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using TabloidMVC.Repositories;
+
+namespace TabloidMVC.Controllers
+{
+
+    [Authorize]
+    public class TagController : Controller
+    {
+        private readonly TagRepository _tagRepository;
+
+        public TagController(IConfiguration config)
+        {
+            _tagRepository = new TagRepository(config);
+        }
+
+        // GET: TagController
+        public IActionResult Index()
+        {
+            var tags = _tagRepository.GetAllTags();
+            return View(tags);
+        }
+
+        // GET: TagController/Details/5
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TagController/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TagController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TagController/Edit/5
+        public IActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TagController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TagController/Delete/5
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TagController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ TabloidMVC/Repositories/TagRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TabloidMVC.Models;
+
+namespace TabloidMVC.Repositories
+{
+    public class TagRepository : BaseRepository
+    {
+        public TagRepository(IConfiguration config) : base(config) { }
+
+        public List<Tag> GetAllTags()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                       SELECT Id, Name
+                       FROM Tag
+                       ORDER BY Name";
+                    var reader = cmd.ExecuteReader();
+
+                    var tags = new List<Tag>();
+
+                    while (reader.Read())
+                    {
+                        tags.Add(NewTagFromReader(reader));
+                    }
+
+                    reader.Close();
+
+                    return tags;
+                }
+            }
+        }
+
+        private Tag NewTagFromReader(SqlDataReader reader)
+        {
+            return new Tag()
+            {
+                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                Name = reader.GetString(reader.GetOrdinal("Name"))
+            };
+        }
+    }
+}
